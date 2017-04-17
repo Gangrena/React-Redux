0f139260e6088e79d6124831bb6e8fe22866ac55
Bartosz Ciurla <bartosz.ciurla@iteo.co>
2017-04-17 23:14:43 +0200

    ADD: New features.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,61 +1,51 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 // How to import all from file
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
+
 
 class CoursesPage extends Component {
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            course: { title: "" }
-        };
-        // this is recomended !!!
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
-
-    onClickSave() {
-        this.props.dispatch(courseActions.createCourse(this.state.course));
-    }
-
-    courseRow(course, index){
-        return <div key={index}>{course.title}</div>;
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-                <input
-                    type="text"
-                    value="Save"
-                    onClick={this.onClickSave} />
-            </div>
-        );
-    }
+  constructor(props, context) {
+    super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
+  }
+  render() {
+    const { courses } = this.props;
+    return (
+      <div>
+        <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage} />
+        <CourseList courses={courses} />
+      </div>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state, ownProps){
-    return {
-        courses: state.courses
-    };
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
