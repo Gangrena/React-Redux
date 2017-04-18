a0f7f560331835b1813b3a43273d10ba8d0b29f4
Bartosz Ciurla <bartosz.ciurla@iteo.co>
2017-04-18 23:12:39 +0200

    ADD: React testing methods.

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -1,14 +1,15 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-function actionTypeEndsIsSuccess(type){
-  return type.substring(type.length -8) == '_SUCCESS';
+function actionTypeEndsIsSuccess(type) {
+  return type.substring(type.length - 8) == '_SUCCESS';
 }
 
-export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action){
-  if(action.type == types.BEGIN_AJAX_CALL){
+export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
+  if (action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
-  }else if(actionTypeEndsIsSuccess(action.type)){
+  } else if (action.type == types.AJAX_CALL_ERROR ||
+    actionTypeEndsIsSuccess(action.type)) {
     return state - 1;
   }
   return state;
